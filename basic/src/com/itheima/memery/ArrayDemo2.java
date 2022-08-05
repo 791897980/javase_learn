eb199294d296e33a38233b5b2225130f19b97912
791897980 <791897980@qq.com>
2022-08-06 07:44:50 +0900

    080601

---
+++ basic/src/com/itheima/memery/ArrayDemo2.java
@@ -0,0 +1,16 @@
+package com.itheima.memery;
+
+public class ArrayDemo2 {
+    public static void main(String[] args) {
+        // 目标：理解俩个数组变量指向同一个数组对象
+        int[] arr1 = {11, 22, 33};
+
+        // 把数组arr1变量赋值给int类型的数组变量arr2
+        int[] arr2 = arr1;
+        System.out.println(arr1);
+        System.out.println(arr2);
+        arr2[1] = 99;
+        System.out.println(arr1[1]);
+        System.out.println(arr2[1]);
+    }
+}
