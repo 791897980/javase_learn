7ba9155d216138c23706921734c4c9ae26b9c143
791897980 <791897980@qq.com>
2022-08-07 03:23:39 +0900

    080601

---
+++ basic/src/com/itheima/create/MethodDemo1.java
@@ -0,0 +1,14 @@
+package com.itheima.create;
+
+public class MethodDemo1 {
+    public static void main(String[] args) {
+        // 目标：学习方法的完整定义格式，并理解其调用和执行流程
+        int rs = add(100, 200);
+        System.out.println("和是：" + rs);
+    }
+
+    public static int add(int a, int b){
+
+        return a + b;
+    }
+}
